f1bc1199452288828812da5532e7a6cdfb471a96
Daisuke Bauza <144384874+daisuke1227@users.noreply.github.com>
2025-04-07 16:46:04 -0400

    Update ModExports.cpp

--- src/ModExports.cpp
+++ src/ModExports.cpp
@@ -1,15 +1,14 @@
-#include <cocos2d.h>
-#include <Geode/modify/CCKeyboardDispatcher.hpp>
-
-// Hack: Temporarily redefine 'private' as 'public' to expose MoreOptionsLayer's members.
+// IMPORTANT: Redefine 'private' as 'public' BEFORE any header that might include MoreOptionsLayer.
 #define private public
 #include <Geode/modify/MoreOptionsLayer.hpp>
 #undef private
 
+#include <cocos2d.h>
+#include <Geode/modify/CCKeyboardDispatcher.hpp>
 #include <Geode/cocos/robtop/keyboard_dispatcher/CCKeyboardDispatcher.h>
 
 // Global pointer to the MoreOptionsLayer instance.
-// This will be set when MoreOptionsLayer is initialized.
+// This will be set when MoreOptionsLayer is created via its init() method.
 MoreOptionsLayer* g_moreOptionsLayerInstance = nullptr;
 
 // Expose CCKeyboardDispatcher::dispatchKeyboardMSG
@@ -35,30 +34,32 @@ extern "C" {
     }
 }
 
-// Create an accessor subclass to expose MoreOptionsLayer::onKeybindings.
-// Now that we've temporarily made private members public, we can use a using-declaration.
+// Define an accessor subclass that exposes MoreOptionsLayer::onKeybindings.
+// Because we redefined private as public when including the header,
+// the formerly private onKeybindings method is now accessible.
 class MoreOptionsLayerAccessor : public MoreOptionsLayer {
 public:
-    using MoreOptionsLayer::onKeybindings; // Make the formerly private onKeybindings accessible.
+    using MoreOptionsLayer::onKeybindings;
 };
 
-// Expose MoreOptionsLayer::onKeybindings via our accessor class
+// Expose MoreOptionsLayer::onKeybindings via our accessor class.
 extern "C" {
     GEODE_EXPORT void MoreOptionsLayer_onKeybindings(cocos2d::CCObject* obj) {
         if (g_moreOptionsLayerInstance) {
-            // Cast the instance to our accessor class and call onKeybindings.
+            // Cast the instance to our accessor subclass and call onKeybindings.
             static_cast<MoreOptionsLayerAccessor*>(g_moreOptionsLayerInstance)->onKeybindings(obj);
         }
     }
 }
 
 // Hook into MoreOptionsLayer's initialization using Geode's $modify macro.
-// This hook will be called when the game creates the MoreOptionsLayer, so we can store its instance.
+// Use the single-parameter form to avoid redefining the class.
 class $modify(MoreOptionsLayer) {
 public:
     bool init() {
         bool ret = MoreOptionsLayer::init();
         if (ret) {
+            // When the layer is initialized, store its instance in the global pointer.
             g_moreOptionsLayerInstance = this;
         }
         return ret;
